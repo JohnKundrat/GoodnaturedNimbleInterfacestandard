263fe7da82f05cc3e8ba60b5698bff0e326e3f7e
John Kundrat <34784536+JohnKundrat@users.noreply.github.com>
2024-11-20 23:59:25 +0000

    Project 11.20.2024

--- main.py
+++ main.py
@@ -1,59 +1,65 @@
 # Prompt user with the Car Shop Welcome Screen
-print("= * 24")
-print("Welcome to the UMBC Car Shop!")
-print("= * 24")
+print("=" * 32)
+print("  Welcome to the UMBC Car Shop!")
+print("=" * 32)
 print("")
-
 
 # Ask the user questions to customize their car and assign to a  variable
 
-print("~Make and Model~")
-print("")
 #Question 1
 
+print("~Make and Model~")
+print("")
 print("1. What Model of Car are you Ordering?")
 print("  a. Lightning Speedster")
 print("  b. Eco Leaf")
 print("  c. Harp Chord")
 print("  d. Chevron")
-
 carMake = input("Please enter (a) - (d): ")
+print("")
 
 # Question 2
 
-print("2. Would you like to upgrade from the 4-door option to the 2-door option?")
-carDoorUpgrade = input("Please enter 'yes' or' 'no'")
+print(
+    "2. Would you like to upgrade from the 4-door option to the 2-door option?"
+)
+carDoorUpgrade = input("Please enter 'yes' or' 'no': ")
 print("")
 
-
-
 # Question 3
-print(" ~ Exterior ~)
-print("Question 3")
+print(" ~ Exterior ~")
+print("")
+print("3. What color would you like your car to be?")
+carColor = input("You may enter the name of any color you'd like: ")
+print("")
 
-      
-Answer3 = input("")
+# Question 4
+print("4. Would you like the deluxe weather package?")
+carWeatherPackage = input("Please enter 'yes' or 'no': ")
+print("")
 
+#Question 5
+print("~ Interior~")
+print("")
+print("5. Which Engine would you like your car to have?")
+print("  a. I-4 Entry Engine")
+print("  b. V-6 Performance Engine")
+print("  c. Eco Diesel Engine")
+carEngine = input("Please enter (a) - (c): ")
+print("")
 
-print("Question 4")
-
-Answer4 = input("")
-
-print("Question 5")
-
-Answer5 = input("")
-
-print("Question 6")
-
-Answer6 = input("")
+#Question 6
+print("6. Would you like heated seats?")
+carHeatedSeats = input("Please enter 'yes' or 'no': ")
+print("")
 
 #Print out summary
 
-
-
-
-
-
-
-
-      
+print("=" * 32)
+print("~ Summary ~")
+print(f"Model Option: {carMake}")
+print(f"Upgrade to 2-Door: {carDoorUpgrade}")
+print(f"Desired Color: {carColor}")
+print(f"Upgrade to Deluxe Weather: {carWeatherPackage}")
+print(f"Engine Option: {carEngine}")
+print(f"Upgrade to Heated Seats: {carHeatedSeats}")
