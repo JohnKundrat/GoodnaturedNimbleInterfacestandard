1ee3d7d46d2198a288fe0afbd43e5db919366808
John Kundrat <37902134-jkundrat44@users.noreply.replit.com>
2024-11-20 23:15:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+# Prompt user with the Car Shop Welcome Screen
+print("= * 24")
+print("Welcome to the UMBC Car Shop!")
+print("= * 24")
+print("")
+
+
+# Ask the user questions to customize their car and assign to a  variable
+
+print("~Make and Model~")
+print("")
+#Question 1
+
+print("1. What Model of Car are you Ordering?")
+print("  a. Lightning Speedster")
+print("  b. Eco Leaf")
+print("  c. Harp Chord")
+print("  d. Chevron")
+
+carMake = input("Please enter (a) - (d): ")
+
+# Question 2
+
+print("2. Would you like to upgrade from the 4-door option to the 2-door option?")
+carDoorUpgrade = input("Please enter 'yes' or' 'no'")
+print("")
+
+
+
+# Question 3
+print(" ~ Exterior ~)
+print("Question 3")
+
+      
+Answer3 = input("")
+
+
+print("Question 4")
+
+Answer4 = input("")
+
+print("Question 5")
+
+Answer5 = input("")
+
+print("Question 6")
+
+Answer6 = input("")
+
+#Print out summary
+
+
+
+
+
+
+
+
+      
